a249b624472e89a939e7b8a705709419b6f4a33e
volodymyr.balashov <volodymyr.balashov@glpgs.com>
2024-05-01 10:13:52 +0900

    basic network setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    kotlin("plugin.serialization") version "1.8.10" apply false
 }
