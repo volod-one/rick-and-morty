4ecbf4d364f1ec5165c62e9f1e545445c3c621c6
volodymyr.balashov <volodymyr.balashov@glpgs.com>
2024-05-01 10:14:08 +0900

    make network call

--- app/src/main/java/one/volod/rickandmorty/MainActivity.kt
+++ app/src/main/java/one/volod/rickandmorty/MainActivity.kt
@@ -8,21 +8,44 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+import one.volod.core.network.Character
+import one.volod.core.network.KtorClient
 import one.volod.rickandmorty.ui.theme.RickandmortyTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var character by remember {
+                mutableStateOf<Character?>(null)
+            }
+
+            LaunchedEffect(
+                key1 = Unit,
+                block = {
+                    delay(3_000)
+                    character = ktorClient.getCharacter(1)
+                }
+            )
+
             RickandmortyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(character?.name ?: "Unknown character")
                 }
             }
         }
