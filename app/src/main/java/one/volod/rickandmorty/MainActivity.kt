b41e58b829c92187ca348d5fb1d81d126c66619b
volodymyr.balashov <volodymyr.balashov@glpgs.com>
2024-05-02 11:16:50 +0900

    create episodes screen

--- app/src/main/java/one/volod/rickandmorty/MainActivity.kt
+++ app/src/main/java/one/volod/rickandmorty/MainActivity.kt
@@ -3,15 +3,10 @@ package one.volod.rickandmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -19,6 +14,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import one.volod.core.network.KtorClient
 import one.volod.rickandmorty.screens.CharacterDetailsScreen
+import one.volod.rickandmorty.screens.CharacterEpisodeScreen
 import one.volod.rickandmorty.ui.theme.RickandmortyTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,7 +49,10 @@ class MainActivity : ComponentActivity() {
                             )
                         ) { backStackEntry ->
                             val characterId = backStackEntry.arguments?.getInt("characterId") ?: -1
-                            CharacterEpisodeScreen(characterId = characterId)
+                            CharacterEpisodeScreen(
+                                characterId = characterId,
+                                ktorClient = ktorClient
+                            )
                         }
                     }
                 }
@@ -62,12 +61,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun CharacterEpisodeScreen(characterId: Int) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(text = "Character episode screen: $characterId", fontSize = 28.sp)
-    }
-}
---
+++ app/src/main/java/one/volod/rickandmorty/screens/CharacterEpisodeScreen.kt
@@ -0,0 +1,113 @@
+package one.volod.rickandmorty.screens
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import one.volod.core.network.KtorClient
+import one.volod.core.network.models.domain.Character
+import one.volod.core.network.models.domain.Episode
+import one.volod.rickandmorty.components.common.CharacterImage
+import one.volod.rickandmorty.components.common.CharacterNameComponent
+import one.volod.rickandmorty.components.common.LoadingState
+import one.volod.rickandmorty.components.episode.EpisodeRowComponent
+
+@Composable
+fun CharacterEpisodeScreen(
+    characterId: Int,
+    ktorClient: KtorClient
+) {
+    var characterState by remember { mutableStateOf<Character?>(null) }
+    var episodesState by remember { mutableStateOf<List<Episode>>(emptyList()) }
+
+    LaunchedEffect(key1 = Unit, block = {
+        ktorClient
+            .getCharacter(characterId)
+            .onSuccess { character ->
+                characterState = character
+                launch {
+                    ktorClient
+                        .getEpisodes(character.episodeIds)
+                        .onSuccess { episodes ->
+                            episodesState = episodes
+                        }.onFailure {
+                            // TODO
+                        }
+                }
+            }.onFailure {
+                // TODO
+            }
+    })
+
+    characterState?.let { character ->
+        MainScreen(character = character, episodes = episodesState)
+    } ?: LoadingState()
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun MainScreen(character: Character, episodes: List<Episode>) {
+    LazyColumn(contentPadding = PaddingValues(all = 16.dp)) {
+        item { CharacterNameComponent(name = character.name) }
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+        item { CharacterImage(imageUrl = character.imageUrl) }
+        item { Spacer(modifier = Modifier.height(32.dp)) }
+
+        episodes.groupBy { it.seasonNumber }.forEach { mapEntry ->
+            stickyHeader { SeasonHeader(seasonNumber = mapEntry.key) }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            items(mapEntry.value) { episode ->
+                EpisodeRowComponent(episode = episode)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+private fun SeasonHeader(seasonNumber: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(top = 8.dp, bottom = 16.dp)
+    ) {
+        Text(
+            text = "Season $seasonNumber",
+            color = Color.White, // RickTextPrimary
+            fontSize = 32.sp,
+            lineHeight = 32.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Color.White, // RickTextPrimary
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(vertical = 4.dp)
+        )
+    }
+}
