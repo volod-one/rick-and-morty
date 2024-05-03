6f104d16762ba2aeb367e80cd9feec0bf819080a
volodymyr.balashov <volodymyr.balashov@glpgs.com>
2024-05-03 21:23:10 +0900

    implement fetching next page with infinity scroll

--- app/src/main/java/one/volod/rickandmorty/screens/HomeScreen.kt
+++ app/src/main/java/one/volod/rickandmorty/screens/HomeScreen.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,12 +27,31 @@ fun HomeScreen(
 ) {
     val viewState by viewModel.viewState.collectAsState()
 
-    LaunchedEffect(key1 = Unit) { viewModel.fetchInitialPage() }
+    LaunchedEffect(key1 = viewModel) { viewModel.fetchInitialPage() }
+
+    val scrollState = rememberLazyGridState()
+    val fetchNextPage: Boolean by remember {
+        derivedStateOf {
+            val currentCharacterCount =
+                (viewState as? HomeScreenViewState.GridDisplay)?.characters?.size
+                    ?: return@derivedStateOf false
+            val lastDisplayedIndex = scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index
+                ?: return@derivedStateOf false
+            return@derivedStateOf lastDisplayedIndex >= currentCharacterCount - 10
+        }
+    }
+
+    LaunchedEffect(key1 = fetchNextPage) {
+        if (fetchNextPage) {
+            viewModel.fetchNextPage()
+        }
+    }
 
     when (val state = viewState) {
         HomeScreenViewState.Loading -> LoadingState()
         is HomeScreenViewState.GridDisplay -> {
             LazyVerticalGrid(
+                state = scrollState,
                 contentPadding = PaddingValues(16.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
--- app/src/main/java/one/volod/rickandmorty/viewmodel/HomeViewModel.kt
+++ app/src/main/java/one/volod/rickandmorty/viewmodel/HomeViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import one.volod.core.network.models.domain.Character
+import one.volod.core.network.models.domain.CharacterPage
 import one.volod.rickandmorty.repositories.CharacterRepository
 import javax.inject.Inject
 
@@ -27,16 +28,33 @@ class HomeScreenViewModel @Inject constructor(
     private val _viewState = MutableStateFlow<HomeScreenViewState>(HomeScreenViewState.Loading)
     val viewState = _viewState.asStateFlow()
 
+    private val fetchedCharacterPages = mutableListOf<CharacterPage>()
+
     fun fetchInitialPage() = viewModelScope.launch {
+        if (fetchedCharacterPages.isNotEmpty()) return@launch
         val initialPage = repository.fetchCharacterPage(page = 1)
         initialPage.onSuccess { characterPage ->
+            fetchedCharacterPages.clear()
+            fetchedCharacterPages.add(characterPage)
+
             _viewState.update { return@update HomeScreenViewState.GridDisplay(characters = characterPage.characters) }
         }.onFailure {
             // TODO
         }
     }
 
-    fun fetchNextPage() {
-        // TODO
+    fun fetchNextPage() = viewModelScope.launch {
+        val nextPageIndex = fetchedCharacterPages.size + 1
+        repository.fetchCharacterPage(page = nextPageIndex).onSuccess { characterPage ->
+            fetchedCharacterPages.add(characterPage)
+
+            _viewState.update { currentState ->
+                val currentCharacters =
+                    (currentState as? HomeScreenViewState.GridDisplay)?.characters ?: emptyList()
+                return@update HomeScreenViewState.GridDisplay(characters = currentCharacters + characterPage.characters)
+            }
+        }.onFailure {
+            // TODO
+        }
     }
 }
